44aaea802609f173b80fec195d0561485097c8be
qnquangg <quang.ng193@gmail.com>
2024-04-27 23:51:19 +0700

    create Row component, then add it to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Main from '../components/Main'
+import Row from '../components/Row'
+import requests from '../Requests'
 
 const Home = () => {
   return (
     <>
-      <Main />  
+      <Main />
+      <Row title='Up Coming' fetchUrl={requests.requestUpcoming} />
+      <Row title='Popular' fetchUrl={requests.requestPopular} />
+      <Row title='Trending' fetchUrl={requests.requestTrending} />
+      <Row title='TopRated' fetchUrl={requests.requestTopRated} />
+      <Row title='Horror' fetchUrl={requests.requestHorror} />
     </>
   )
 }
