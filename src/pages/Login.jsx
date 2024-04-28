c7a1535431027a9e63d185fd2d36da1615fcb56b
qnquangg <quang.ng193@gmail.com>
2024-04-28 22:29:28 +0700

    change the navbar and update code for login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { UserAuth } from "../context/AuthContext";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { user, logIn } = UserAuth();
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await logIn(email, password);
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <div className="w-full h-screen">
       <img
@@ -12,36 +31,38 @@ const Login = () => {
       <div className="fixed w-full px-4 py-24 z-50">
         <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
           <div className="max-w-[320px] mx-auto py-16">
-            <h1 className="text-3xl font-bold">Sign Up</h1>
-            <form className="w-full flex flex-col py-4">
+            <h1 className="text-3xl font-bold">Sign In</h1>
+            <form onSubmit={handleSubmit} className="w-full flex flex-col py-4">
               <input
+                onChange={(e) => setEmail(e.target.value)}
                 className="p-3 my-3 bg-gray-700 rounded"
                 type="email"
                 placeholder="Email"
                 autoComplete="email"
               />
               <input
+                onChange={(e) => setPassword(e.target.value)}
                 className="p-3 my-3 bg-gray-700 rounded"
                 type="password"
                 placeholder="Password"
                 autoComplete="current-password"
               />
-            </form>
-            <button className="w-full py-2 my-6 bg-red-600 rounded font-bold">
-              Sign In
-            </button>
-            <div className="flex justify-between items-center text-sm text-gray-600">
-              <p>
-                <input className="mr-2" type="checkbox" /> Remember me{" "}
+
+              <button className="w-full py-2 my-6 bg-red-600 rounded font-bold">
+                Sign In
+              </button>
+              {error && <p className="text-red-500">{error}</p>}
+              <div className="flex justify-between items-center text-sm text-gray-600">
+                <p>
+                  <input className="mr-2" type="checkbox" /> Remember me{" "}
+                </p>
+                <p>Need help?</p>
+              </div>
+              <p className="py-4">
+                <span className="text-gray-600">New to Netflix? </span>
+                <Link to="/signup">Sign In</Link>
               </p>
-              <p>Need help?</p>
-            </div>
-            <p className="py-4">
-              <span className="text-gray-600">
-                New to Netflix?{" "}
-              </span>
-              <Link to="/signup">Sign Up</Link>
-            </p>
+            </form>
           </div>
         </div>
       </div>
