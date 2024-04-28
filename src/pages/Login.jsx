3f9cd4ca73eb5c06220cbf533695ebca15a99200
qnquangg <quang.ng193@gmail.com>
2024-04-28 21:17:13 +0700

    Finish UI for SignIn and SignUp

---
+++ src/pages/Login.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="w-full h-screen">
+      <img
+        className="hidden sm:block absolute w-full h-full object-cover"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/058eee37-6c24-403a-95bd-7d85d3260ae1/71225a91-df69-403f-a635-2d24310f19a6/VN-en-20240422-POP_SIGNUP_TWO_WEEKS-perspective_WEB_b7380b7a-6c17-456a-8c16-97d433fde0e7_medium.jpg"
+      />
+      <div className="bg-black/60 fixed top-0 left-0 w-full h-screen" />
+      <div className="fixed w-full px-4 py-24 z-50">
+        <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+          <div className="max-w-[320px] mx-auto py-16">
+            <h1 className="text-3xl font-bold">Sign Up</h1>
+            <form className="w-full flex flex-col py-4">
+              <input
+                className="p-3 my-3 bg-gray-700 rounded"
+                type="email"
+                placeholder="Email"
+                autoComplete="email"
+              />
+              <input
+                className="p-3 my-3 bg-gray-700 rounded"
+                type="password"
+                placeholder="Password"
+                autoComplete="current-password"
+              />
+            </form>
+            <button className="w-full py-2 my-6 bg-red-600 rounded font-bold">
+              Sign In
+            </button>
+            <div className="flex justify-between items-center text-sm text-gray-600">
+              <p>
+                <input className="mr-2" type="checkbox" /> Remember me{" "}
+              </p>
+              <p>Need help?</p>
+            </div>
+            <p className="py-4">
+              <span className="text-gray-600">
+                New to Netflix?{" "}
+              </span>
+              <Link to="/signup">Sign Up</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
